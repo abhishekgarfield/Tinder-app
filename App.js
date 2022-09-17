ccba8aed900174a5b12e8990512d62b1293510d7
abhishekgarfield <abhishek2759@gmail.com>
2022-09-17 19:11:16 +0530

    error

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { View, Text } from "react-native";
-import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { View, Text, KeyboardAvoidingView, Platform } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Chatscreen from "./Screens/Chatscreen";
@@ -7,30 +7,21 @@ import Homescreen from "./Screens/Homescreen";
 import { AuthProvider } from "./Hooks/useAuth";
 import LoginScreen from "./Screens/LoginScreen";
 import Login from "./Screens/Login";
+import Signup from "./Screens/signup"
 
 export default function App() {
   const Stack = createStackNavigator();
-  const User = false;
+  const User = true;
   return (
     <NavigationContainer>
-      <SafeAreaProvider>
-        <AuthProvider>
-          {/* HOC  -high order component */}
-          <Stack.Navigator>
-            {User ? (
-              <>
-                <Stack.Screen name="homescreen" component={Homescreen} />
-                <Stack.Screen name="chatscreen" component={Chatscreen} />
-              </>
-            ) : (
-              <>
-              <Stack.Screen name="loginscreen" component={LoginScreen} />
-              <Stack.Screen name="login" component={Login}/>
-              </>
-            )}
-          </Stack.Navigator>
-        </AuthProvider>
-      </SafeAreaProvider>
+            {/* HOC  -high order component */}
+            <Stack.Navigator>
+             
+                  <Stack.Screen name="homescreen" component={Homescreen} />
+                  <Stack.Screen name="chatscreen" component={Chatscreen} />
+              
+              
+            </Stack.Navigator>
     </NavigationContainer>
   );
 }
