465e4dc271996a1a3ede734ddd55766161109149
abhishekgarfield <abhishek2759@gmail.com>
2022-09-23 20:04:54 +0530

    messages

--- Components/messages.js
+++ Components/messages.js
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import { useSelector } from "react-redux";
 const Messages = ({ selecteduser }) => {
   const user = useSelector((state) => {
     return state.user.userinfo;
   });
+  const [Messages, setMessages] = useState(null);
+
+  const getMessages = () => {
+    const url = `http://localhost:8000/messages?currentuserid=${user.user_id}&selecteduserid=${selecteduser.user_id}`;
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  };
+  useEffect(() => {
+    getMessages();
+  }, []);
   return (
     <View style={{ flexGrow: 1 }}>
       <Text>{selecteduser.first_name}</Text>
